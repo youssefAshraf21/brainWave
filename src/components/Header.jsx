538323c157c66342b681e704c947bf7252a9e303
youssefAshraf21 <ya614962@gmail.com>
2026-06-27 20:22:42 +0300

    feat: update Header component to adjust logo link width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
     return (
         <div className= {`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8' : 'bg-n-8/90  backdrop-blur-sm'}`}>
             <div className='flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 '>
-                <a className='block w-[12rem] xl:mr-8' href="#hero">
+                <a className='block w-48 xl:mr-8' href="#hero"> 
                     <img src={brainwave} alt="BrainWave" width={190} height={40} />
                 </a>
                         <nav
